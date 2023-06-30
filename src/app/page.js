c67a2d4fdc7c0d90fbcaa0b5188f265f50653cef
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-06-30 09:00:41 +0530

    Update Content

--- src/app/page.js
+++ src/app/page.js
@@ -17,8 +17,8 @@ export default function Home() {
 
       <div className="container mx-auto flex justify-between py-5 border-b">
           <div className="left flex gap-3">
-              <button className='flex bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-grary-50 hover:border-indigo-500 hover:text-gray-800'>
-                Add Employee <span className='px-1'><BiUserPlus size={23}></BiUserPlus></span>
+              <button className='flex bg-indigo-500 text-white font-bold px-4 py-2 border rounded-md hover:bg-grary-50 hover:border-indigo-500 hover:text-gray-800'>
+                Add Employee <span className='px-3 font-bold'><BiUserPlus size={23}></BiUserPlus></span>
               </button>
           </div>
       </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -95,8 +95,8 @@ export const Form = () => {
         </div>
       </div>
 
-      <button className="flex justify-center text-md w-2/6 bg-green-500 text-white px-4 py-2 border rounded-md hover:bg-gray-50 hover:border-green-500 hover:text-green-500">
-             Add <span className="px-1"><BiPlus size={24}></BiPlus></span>
+      <button className="flex justify-center text-md w-2/6 bg-green-500 text-white px-4 py-2 border rounded-md font-bold hover:bg-gray-50 hover:border-green-500 hover:text-green-500">
+             Add <span className="px-3 font-bold"><BiPlus size={24}></BiPlus></span>
             </button>
     </form>
   );
