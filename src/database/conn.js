59bd2fe024bbfdb2cc4408698e42da798e63b81a
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-06-30 11:40:03 +0530

    Update Content

---
+++ src/database/conn.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+import { config } from "dotenv";
+// dotenv configuration
+config();
+
+const connectMongo = async () => {
+  try {
+    const { connection } = await mongoose.connect(process.env.MongoDB_URL);
+
+    if (connection.readyState == 1) {
+      console.log("Database Connected");
+    }
+  } catch (errors) {
+    return Promise.reject(errors);
+  }
+};
+
+export default connectMongo;
