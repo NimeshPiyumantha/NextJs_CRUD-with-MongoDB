6a8bb0fbe2da1be25f11602770da3ea0bd8b1b5d
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-06-30 11:42:12 +0530

    Update Content

--- src/api/hello.js
+++ src/api/hello.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import connectMongo from "../../database/conn";
+import connectMongo from "../database/conn";
 
 export default function handler(req, res) {
   connectMongo();
