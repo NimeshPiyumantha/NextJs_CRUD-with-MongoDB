be5e9f7747a5cb0f0e068590aaf7d6e3b67a3cce
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-07-01 18:52:37 +0530

    Update Content

--- src/database/controller.js
+++ src/database/controller.js
@@ -1,13 +1,27 @@
-/** Controller */
-import Users from "../model/user";
+import Users from '../model/user';
 
+// get : http://localhost:3000/api/users
 export async function getUsers(req, res) {
   try {
     const users = await Users.find({});
 
-    if (!users) return res.status(404).json({ error: "Data not Found" });
+    if (!users) return res.status(404).json({ error: "Data not found" });
     res.status(200).json(users);
   } catch (error) {
-    res.status(404).json({ error: "Error While Fetching Data" });
+    res.status(404).json({ error: "Error while fetching data" });
+  }
+}
+
+// post : http://localhost:3000/api/users
+export async function postUser(req, res) {
+  try {
+    const formData = req.body;
+    if (!formData)
+      return res.status(404).json({ error: "Form data not provided" });
+
+    const data = await Users.create(formData);
+    return res.status(200).json(data);
+  } catch (error) {
+    return res.status(404).json({ error });
   }
 }
--- src/pages/api/users/index.js
+++ src/pages/api/users/index.js
@@ -1,5 +1,5 @@
 import connectMongo from "../../../database/conn";
-import { getUsers } from "../../../database/controller";
+import { getUsers, postUser } from "../../../database/controller";
 
 export default async function handler(req, res) {
   connectMongo().catch(() =>
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
       getUsers(req, res);
       break;
     case "POST":
-      res.status(200).json({ method, name: "POST Request" });
+      postUser(req, res);
       break;
     case "PUT":
       res.status(200).json({ method, name: "PUT Request" });
