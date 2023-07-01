9965908a14e7da01665410b33b31bf0478872dd9
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-07-01 22:08:01 +0530

    Update Content

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
-import Reducer from './reducer'
-import listenerMiddleware from './listener';
+import { configureStore } from "@reduxjs/toolkit";
+import Reducer from "./reducer";
+import listenerMiddleware from "./listener";
 
 export const store = configureStore({
-    reducer : {
-        app : Reducer
-    },
-    middleware : (getDefaultMiddleware) => getDefaultMiddleware().prepend(listenerMiddleware.middleware)
-})
+  reducer: {
+    app: Reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().prepend(listenerMiddleware.middleware),
+});
