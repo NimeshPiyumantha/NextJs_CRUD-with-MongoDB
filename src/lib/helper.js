32ab7eaaa0535f5d9a680f6fcc8bab304ecb38bf
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-07-01 20:27:47 +0530

    Add posting a new user

--- src/lib/helper.js
+++ src/lib/helper.js
@@ -16,4 +16,21 @@ export const getUser = async (userId) => {
   return {};
 };
 
+// posting a new user
+export async function addUser(formData) {
+  try {
+    const Options = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formData),
+    };
+
+    const response = await fetch(`${BASE_URL}api/users`, Options);
+    const json = await response.json();
+
+    return json;
+  } catch (error) {
+    return error;
+  }
+}
 
