142126c9605003a53cac4fd827b2c850c8aefa75
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-07-01 19:38:21 +0530

    Update Content

--- src/database/controller.js
+++ src/database/controller.js
@@ -1,4 +1,4 @@
-import Users from '../model/user';
+import Users from "../model/user";
 
 // get : http://localhost:3000/api/users
 export async function getUsers(req, res) {
@@ -9,6 +9,25 @@ export async function getUsers(req, res) {
     res.status(200).json(users);
   } catch (error) {
     res.status(404).json({ error: "Error while fetching data" });
+  }
+}
+
+// get : http://localhost:3000/api/users/1
+export async function getUser(req, res) {
+  try {
+    const { userId } = req.query;
+
+    if (userId) {
+      const user = await Users.findById(userId);
+      if (!user) {
+        return res.status(404).json({ error: "User not found" });
+      }
+      return res.status(200).json(user);
+    } else {
+      return res.status(400).json({ error: "User ID not provided" });
+    }
+  } catch (error) {
+    return res.status(404).json({ error: "Cannot get the User" });
   }
 }
 
@@ -23,5 +42,46 @@ export async function postUser(req, res) {
     return res.status(200).json(data);
   } catch (error) {
     return res.status(404).json({ error });
+  }
+}
+
+// put : http://localhost:3000/api/users/1
+export async function putUser(req, res) {
+  try {
+    const { userId } = req.query;
+    const formData = req.body;
+
+    if (userId && formData) {
+      const user = await Users.findByIdAndUpdate(userId, formData, {
+        new: true,
+      });
+      if (!user) {
+        return res.status(404).json({ error: "User not found" });
+      }
+      return res.status(200).json(user);
+    }
+
+    return res.status(400).json({ error: "User ID or form data not provided" });
+  } catch (error) {
+    return res.status(500).json({ error: "Error while updating the data" });
+  }
+}
+
+// delete : http://localhost:3000/api/users/1
+export async function deleteUser(req, res) {
+  try {
+    const { userId } = req.query;
+
+    if (userId) {
+      const user = await Users.findByIdAndDelete(userId);
+      if (!user) {
+        return res.status(404).json({ error: "User not found" });
+      }
+      return res.status(200).json(user);
+    }
+
+    return res.status(400).json({ error: "User ID not provided" });
+  } catch (error) {
+    res.status(404).json({ error: "Error while deleting the user" });
   }
 }
--- src/pages/api/users/[userId].js
+++ src/pages/api/users/[userId].js
@@ -1,10 +1,5 @@
 import connectMongo from "../../../database/conn";
-import {
-  getUsers,
-  postUser,
-  putUser,
-  deleteUser,
-} from "../../../database/controller";
+import { getUser, putUser, deleteUser } from "../../../database/controller";
 
 export default async function handler(req, res) {
   connectMongo().catch(() =>
@@ -16,10 +11,7 @@ export default async function handler(req, res) {
 
   switch (method) {
     case "GET":
-      getUsers(req, res);
-      break;
-    case "POST":
-      postUser(req, res);
+      getUser(req, res);
       break;
     case "PUT":
       putUser(req, res);
