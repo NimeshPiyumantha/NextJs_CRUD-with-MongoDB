16b40ded1a3572b02a7b70be9bc8d75bd97edd04
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-07-01 20:27:24 +0530

    Add single user api

--- src/lib/helper.js
+++ src/lib/helper.js
@@ -1,8 +1,19 @@
 const BASE_URL = "http://localhost:3000";
-
+// all user
 export const getUsers = async () => {
   const response = await fetch(`${BASE_URL}/api/users`);
   const json = await response.json();
 
   return json;
 };
+
+// single user
+export const getUser = async (userId) => {
+  const response = await fetch(`${BASE_URL}api/users/${userId}`);
+  const json = await response.json();
+
+  if (json) return json;
+  return {};
+};
+
+
